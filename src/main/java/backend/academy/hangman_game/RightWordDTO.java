a515f7cbc4053e659ca9a8fdeb7e95e4cc7f959e
vodoem <prosto_duz@mail.ru>
2024-09-06 14:43:59 +0300

    Был добавлен конструктор и геттеры класса RightWordDTO

--- src/main/java/backend/academy/hangman_game/RightWordDTO.java
+++ src/main/java/backend/academy/hangman_game/RightWordDTO.java
@@ -1,8 +1,37 @@
 package backend.academy.hangman_game;
 
 public class RightWordDTO {
-    private String word;
-    private String hint;
-    private WordDifficultyLevel wordDifficultyLevel;
-    private WordCategoty wordCategoty;
+    private final String word;
+    private final String hint;
+    private final WordDifficultyLevel wordDifficultyLevel;
+    private final WordCategoty wordCategoty;
+
+    public RightWordDTO(String word, String hint, WordCategoty wordCategoty) {
+        this.word = word;
+        this.hint = hint;
+        this.wordCategoty = wordCategoty;
+        if (word.length() < 5) {
+            wordDifficultyLevel = WordDifficultyLevel.Easy;
+        } else if (word.length() < 8) {
+            wordDifficultyLevel = WordDifficultyLevel.Medium;
+        } else {
+            wordDifficultyLevel = WordDifficultyLevel.Hard;
+        }
+    }
+
+    public WordCategoty getWordCategoty() {
+        return wordCategoty;
+    }
+
+    public WordDifficultyLevel getWordDifficultyLevel() {
+        return wordDifficultyLevel;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public String getWord() {
+        return word;
+    }
 }
