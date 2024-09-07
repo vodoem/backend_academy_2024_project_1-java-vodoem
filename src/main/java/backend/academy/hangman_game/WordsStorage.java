e9f54778dbea5b15b434b3a7574dbdc5063a7097
vodoem <prosto_duz@mail.ru>
2024-09-07 22:52:33 +0300

    Были добавлены методы initializeGameWords, getGameWords, getRandomWordWithChosenCategoryAndDifficultLevel и getGameWordsWithChosenCategoryAndDifficultLevel в классе WordsStorage

--- src/main/java/backend/academy/hangman_game/WordsStorage.java
+++ src/main/java/backend/academy/hangman_game/WordsStorage.java
@@ -1,11 +1,41 @@
 package backend.academy.hangman_game;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 
 public class WordsStorage {
-    private static List<String> gameWords;
+    private static List<RightWordDTO> gameWords;
 
+    public static void initializeGameWords(List<RightWordDTO> gameWords){
+        WordsStorage.gameWords = gameWords;
+    }
 
+    public static List<RightWordDTO> getGameWords() {
+        return gameWords;
+    }
+
+    public static RightWordDTO getRandomWordWithChosenCategoryAndDifficultLevel(
+        WordCategoty wordCategoty,
+        WordDifficultyLevel wordDifficultyLevel
+    ) {
+        Random randomizer = new Random();
+        List<RightWordDTO> list =
+            WordsStorage.getGameWordsWithChosenCategoryAndDifficultLevel(wordCategoty, wordDifficultyLevel);
+        return list.get(randomizer.nextInt(list.size()));
+    }
+
+    private static List<RightWordDTO> getGameWordsWithChosenCategoryAndDifficultLevel(
+        WordCategoty wordCategoty,
+        WordDifficultyLevel wordDifficultyLevel
+    ) {
+        List<RightWordDTO> resultList = new ArrayList<>();
+        for (RightWordDTO word : WordsStorage.gameWords) {
+            if (word.getWordCategoty() == wordCategoty && word.getWordDifficultyLevel() == wordDifficultyLevel) {
+                resultList.add(word);
+            }
+        }
+        return resultList;
+    }
 
 }
