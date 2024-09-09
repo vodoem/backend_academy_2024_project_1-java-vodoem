b8256f8952649e3bf005132cdda3f9ec4582cd63
vodoem <prosto_duz@mail.ru>
2024-09-09 15:01:56 +0300

    Был создан класс HangmanFigure, содержащий в себе стадии отрисовки модельки висельника и метода по возвращанию этапа отрисовки модели по переданному количеству ошибок

--- src/main/java/backend/academy/hangman_game/Main.java
+++ src/main/java/backend/academy/hangman_game/Main.java
@@ -1,9 +1,11 @@
 package backend.academy.hangman_game;
 
 import lombok.experimental.UtilityClass;
+import java.io.IOException;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+
     }
 }
